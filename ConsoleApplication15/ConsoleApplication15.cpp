02b2fe2bea5d91ffb9d1ddd1b1892a14bac959c5
KDG <delovoykdg@gmail.com>
2025-03-05 18:02:40 +0200

    код сервер и клиент

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define PORT 49151
+#define DEFAULT_BUFLEN 512
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cout << "Ошибка WSAStartup!" << endl;
+        return 1;
+    }
+
+    SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ListenSocket == INVALID_SOCKET) {
+        cout << "Ошибка создания сокета: " << WSAGetLastError() << endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+
+    if (bind(ListenSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cout << "Ошибка привязки: " << WSAGetLastError() << endl;
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+        cout << "Ошибка прослушивания: " << WSAGetLastError() << endl;
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Сервер запущен! Ожидание подключения..." << endl;
+
+    SOCKET ClientSocket = accept(ListenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET) {
+        cout << "Ошибка accept: " << WSAGetLastError() << endl;
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Клиент подключен." << endl;
+
+    char recvBuffer[DEFAULT_BUFLEN];
+    while (true) {
+        memset(recvBuffer, 0, DEFAULT_BUFLEN);
+        int recvResult = recv(ClientSocket, recvBuffer, DEFAULT_BUFLEN, 0);
+
+        if (recvResult > 0) {
+            int number = atoi(recvBuffer);
+            number++;
+            string response = to_string(number);
+            send(ClientSocket, response.c_str(), response.size(), 0);
+        }
+        else {
+            cout << "Клиент отключился." << endl;
+            break;
+        }
+    }
+
+    closesocket(ClientSocket);
+    closesocket(ListenSocket);
+    WSACleanup();
+    return 0;
+}
---
+++ ConsoleApplication16/ConsoleApplication16.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+#include <windows.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define PORT 49151
+#define SERVER_IP "127.0.0.1"
+
+using namespace std;
+
+bool isServerRunning() {
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) return false;
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr);
+
+    bool connected = (connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) != SOCKET_ERROR);
+    closesocket(sock);
+    return connected;
+}
+
+void startServer() {
+    STARTUPINFO si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+
+    if (!CreateProcess(L"Server.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+        cout << "Ошибка запуска сервера!" << endl;
+    }
+    else {
+        cout << "Сервер запущен!" << endl;
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cout << "Ошибка WSAStartup!" << endl;
+        return 1;
+    }
+
+    if (!isServerRunning()) {
+        cout << "Сервер не найден, запускаем..." << endl;
+        startServer();
+        Sleep(2000); // Ждём 2 сек, чтобы сервер успел запуститься
+    }
+
+    SOCKET ClientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (ClientSocket == INVALID_SOCKET) {
+        cout << "Ошибка создания сокета!" << endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr);
+
+    if (connect(ClientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cout << "Ошибка подключения!" << endl;
+        closesocket(ClientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Подключено к серверу!" << endl;
+
+    string input;
+    while (true) {
+        cout << "Введите число (или 'exit' для выхода): ";
+        cin >> input;
+
+        if (input == "exit") break;
+
+        send(ClientSocket, input.c_str(), input.size(), 0);
+        char buffer[512] = { 0 };
+        recv(ClientSocket, buffer, sizeof(buffer), 0);
+        cout << "Ответ от сервера: " << buffer << endl;
+    }
+
+    closesocket(ClientSocket);
+    WSACleanup();
+    return 0;
+}
